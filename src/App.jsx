beb932dfe9c126f5179ff735bf934d89c326bf7e
JunhyV <jonhy.kisi@gmail.com>
2023-02-07 11:03:12 -0600

    add lorem

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia earum totam, non repudiandae dolor debitis iste optio dolorem ex ipsa iusto, quos odio qui, vero eum? Ex ad odit et.</h1>
+      <h2>Lorem ipsum dolor sit amet.</h2>
     </div>
   )
 }
