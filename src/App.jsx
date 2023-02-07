de38bf2c6eb416f6d35bbe97bd3d42320bc8355b
JunhyV <jonhy.kisi@gmail.com>
2023-02-07 10:59:57 -0600

    first

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae officia autem magnam earum, itaque totam explicabo nesciunt sed neque blanditiis suscipit ex nulla ipsa consectetur praesentium fugit perferendis facere odit?</h1>
     </div>
   )
 }
